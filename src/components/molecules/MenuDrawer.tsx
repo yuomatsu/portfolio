91fb1d7568353e71a675f88d4539a527d2dfd02c
yuomatsu <07yuya08@gmail.com>
2022-02-23 18:52:43 +0900

    ヘッダールーティング設定

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -4,19 +4,23 @@ import { Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakr
 type Props = {
     onClose: () => void;
     isOpen: boolean;
+    onClickHome: () => void;
+    onClickProfile: () => void;
+    onClickSkill: () => void;
+    onClickCareer: () => void;
 }
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-    const { onClose, isOpen } = props;
+    const { onClose, isOpen, onClickHome, onClickProfile, onClickSkill, onClickCareer } = props;
     return (
         <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay>
             <DrawerContent>
                 <DrawerBody p={0} bg="gray.100">
-                    <Button w="100%">HOME</Button>
-                    <Button w="100%">PROFILE</Button>
-                    <Button w="100%">SKILL</Button>
-                    <Button w="100%">CAREER</Button>
+                    <Button w="100%" onClick={onClickHome}>HOME</Button>
+                    <Button w="100%" onClick={onClickProfile}>PROFILE</Button>
+                    <Button w="100%" onClick={onClickSkill}>SKILL</Button>
+                    <Button w="100%" onClick={onClickCareer}>CAREER</Button>
                 </DrawerBody>
             </DrawerContent>
         </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,19 @@
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
 
 import {MenuIconButton} from "../../atoms/button/MenuIconButton"
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useHistory } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
     const {isOpen, onOpen, onClose} = useDisclosure();
+    const history = useHistory();
+
+    const onClickHome = useCallback(() => history.push("/"), []);
+    const onClickProfile = useCallback(() => history.push("/profile"), []);
+    const onClickSkill = useCallback(() => history.push("/skill"), []);
+    const onClickCareer = useCallback(() => history.push("/career"), []);
+
     return (
         <>
         <Flex
@@ -17,27 +25,33 @@ export const Header: VFC = memo(() => {
             padding={{base: 3, md: 5 }}
         >
             <Flex as="a" _hover={{cursor: "pointer"}}>
-                <Heading as="h1" fontSize={{ base: "md", md: 25 }}>
+                <Heading as="h1" fontSize={{ base: "lg", md: 25 }} onClick={onClickHome}>
                     yuomatsu
                 </Heading>
             </Flex>
             <Flex align="center" fontSize="sm" display={{ base: "none", md: "flex" }}>
                 <Box pr={4}>
-                    <Link>HOME</Link>
+                    <Link onClick={onClickHome}>HOME</Link>
                 </Box>
                 <Box pr={4}>
-                    <Link>PLOFILE</Link>
+                    <Link onClick={onClickProfile}>PLOFILE</Link>
                 </Box>
                 <Box pr={4}>
-                    <Link>SKILL</Link>
+                    <Link onClick={onClickSkill}>SKILL</Link>
                 </Box>
                 <Box pr={4}>
-                    <Link>CAREER</Link>
+                    <Link onClick={onClickCareer}>CAREER</Link>
                 </Box>
             </Flex>
             <MenuIconButton onOpen={onOpen}/>
         </Flex>
-        <MenuDrawer onClose={onClose} isOpen={isOpen}/>
+        <MenuDrawer
+            onClose={onClose}
+            isOpen={isOpen}
+            onClickHome={onClickHome}
+            onClickProfile={onClickProfile}
+            onClickSkill={onClickSkill}
+            onClickCareer={onClickCareer}/>
         </>
         
     )
