c34430d4171924b96efe2e175d6d9232b48b597b
yuomatsu <07yuya08@gmail.com>
2022-02-23 21:51:17 +0900

    HOME作成開始

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,16 @@
 import { memo, VFC } from "react";
+import { Box } from "@chakra-ui/layout";
+import { Image, Text } from '@chakra-ui/react';
+
+// import { imgKobe1 } from "../../img/kobe1_mono.jpg";
 
 export const Home: VFC = memo(() => {
-    return <p>HPOMEページです</p>
+    return (
+        <Box margin={{ base: 5, md: 20}} textAlign="center" backgroundColor="gray.300">
+            <Text fontSize='5xl'>Welcome to my portfolio!</Text>
+            <Box>
+                {/* <Image src={imgKobe1} alt="神戸"/> */}
+            </Box>
+        </Box>
+    )
 }) 
