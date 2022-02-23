3989f92af5e68ff09548e3cb3a4da109e054573e
yuomatsu <07yuya08@gmail.com>
2022-02-23 14:09:31 +0900

    ヘッダー作成途中

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,24 +6,25 @@ import {Profile} from "../components/pages/Profile"
 import {Skill} from "../components/pages/Skill"
 import { Career } from "../components/pages/Career";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: VFC = memo(() => {
     return (
         <Switch>
             <Route exact path="/">
-                <Home />
+                <HeaderLayout><Home /></HeaderLayout>
             </Route>
             <Route exact path="/profile">
-                <Profile />
+                <HeaderLayout><Profile /></HeaderLayout>
             </Route>
             <Route exact path="/skill">
-                <Skill />
+                <HeaderLayout><Skill /></HeaderLayout>
             </Route>
-            <Route exact path="/carrer">
-                <Career />
+                <Route exact path="/career">
+                <HeaderLayout><Career /></HeaderLayout>
             </Route>
             <Route exact path="*">
-                <Page404 />
+                <HeaderLayout><Page404 /></HeaderLayout>
             </Route>
         </Switch>
     )
