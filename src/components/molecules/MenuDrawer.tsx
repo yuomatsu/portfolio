4b66a11d246edf55cbca426be9cc0e5e75c12021
yuomatsu <07yuya08@gmail.com>
2022-02-23 18:27:11 +0900

    ヘッダーコンポーネント化

---
+++ src/components/molecules/MenuDrawer.tsx
@@ -0,0 +1,26 @@
+import { memo, VFC } from "react";
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakra-ui/react";
+
+type Props = {
+    onClose: () => void;
+    isOpen: boolean;
+}
+
+export const MenuDrawer: VFC<Props> = memo((props) => {
+    const { onClose, isOpen } = props;
+    return (
+        <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+            <DrawerContent>
+                <DrawerBody p={0} bg="gray.100">
+                    <Button w="100%">HOME</Button>
+                    <Button w="100%">PROFILE</Button>
+                    <Button w="100%">SKILL</Button>
+                    <Button w="100%">CAREER</Button>
+                </DrawerBody>
+            </DrawerContent>
+        </DrawerOverlay>
+
+    </Drawer>
+    )
+}) 
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,6 +1,8 @@
 import { memo, VFC } from "react";
-import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure } from "@chakra-ui/react";
-import {HamburgerIcon} from "@chakra-ui/icons"
+import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+
+import {MenuIconButton} from "../../atoms/button/MenuIconButton"
+import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
     const {isOpen, onOpen, onClose} = useDisclosure();
@@ -15,11 +17,11 @@ export const Header: VFC = memo(() => {
             padding={{base: 3, md: 5 }}
         >
             <Flex as="a" _hover={{cursor: "pointer"}}>
-                <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+                <Heading as="h1" fontSize={{ base: "md", md: 25 }}>
                     yuomatsu
                 </Heading>
             </Flex>
-            <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
+            <Flex align="center" fontSize="sm" display={{ base: "none", md: "flex" }}>
                 <Box pr={4}>
                     <Link>HOME</Link>
                 </Box>
@@ -33,24 +35,10 @@ export const Header: VFC = memo(() => {
                     <Link>CAREER</Link>
                 </Box>
             </Flex>
-                <IconButton aria-label="メニューボタン" icon={<HamburgerIcon />}
-                size="lg" variant="unstyled" display={{ base: "block", md: "none"}}
-                onClick={onOpen}
-                />
+            <MenuIconButton onOpen={onOpen}/>
         </Flex>
-        <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-            <DrawerOverlay>
-                <DrawerContent>
-                    <DrawerBody p={0} bg="gray.100">
-                        <Button w="100%">HOME</Button>
-                        <Button w="100%">PROFILE</Button>
-                        <Button w="100%">SKILL</Button>
-                        <Button w="100%">CAREER</Button>
-                    </DrawerBody>
-                </DrawerContent>
-            </DrawerOverlay>
-
-        </Drawer>
+        <MenuDrawer onClose={onClose} isOpen={isOpen}/>
         </>
+        
     )
 }) 
