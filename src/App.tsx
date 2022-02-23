f91fac5844c1bc5bea06db9b5240b9d4bb3efff6
yuomatsu <07yuya08@gmail.com>
2022-02-23 12:31:23 +0900

    ChakraUIを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {Button, ChakraProvider} from "@chakra-ui/react"
+
+import theme from "./theme/theme"
 
 function App() {
   return (
-    <p>Hello World</p>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload!!!
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <ChakraProvider theme={theme}>
+      <p>Hello World</p>
+      <Button>ボタン</Button>
+    </ChakraProvider> 
   );
 }
 
