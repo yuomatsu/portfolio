3303f3357a33cbccd1645f3e3baee2501b038fb8
yuomatsu <07yuya08@gmail.com>
2022-02-26 00:05:35 +0900

    写真レイアウトを修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,13 +6,14 @@ import { Image, Text } from '@chakra-ui/react';
 
 export const Home: VFC = memo(() => {
     return (
-        <Box mt={{ base: 5, md: 20}} mb={{ base: 5, md: 20}} mr={{ base: 5, md: "auto"}} ml={{ base: 5, md: "auto"}} textAlign="center" backgroundColor="gray.300" maxW="1200px">
+        <Box mt={{ base: 5, md: 120}} mb={{ base: 5, md: 20}} mr={{ base: 5, md: "auto"}} ml={{ base: 5, md: "auto"}} textAlign="center" backgroundColor="gray.300" maxW="1200px">
             <Text fontSize='5xl'>Welcome to my portfolio!</Text>
             <Text fontSize='5xl'>Thank you!</Text>
-            <Box m={{ base:5, md: 20}}　w="100%" position={{ base: "absolute", md: "relative"}}>
-                {/* <Image src={imgKobe1} alt="神戸"/> */}
-                <Image w={{ base: "100%", md: "700px"}} position={{ base: "static", md: "absolute"}} top={{ base: "auto", md: 0}} right={{ base: "auto", md: 0}} src={`${process.env.PUBLIC_URL}/img/kobe1_mono.jpg`} alt="神戸のスタバ"/>
-                <Image w={{ base: "100%", md: "700px"}} position={{ base: "static", md: "absolute"}} top={{ base: "auto", md: "400px"}} left={{ base: "auto", md: 0}} src={`${process.env.PUBLIC_URL}/img/kobe2_mono.jpg`} alt="神戸の夜景"/>
+            <Box mt={{ base:5, md: 120}} w="100%" h="900px" position={{ base: "static", md: "relative"}}>
+                {/* <Box position={{ base: "static", md: "relative"}} w="100%" h="700px"> */}
+                <Image w={{ base: "100%", md: "650px"}} position={{ base: "static", md: "absolute"}} top={{ base: "auto", md: 0}} right={{ base: "auto", md: 10}} src={`${process.env.PUBLIC_URL}/img/kobe1_mono.jpg`} alt="神戸のスタバ"/>
+                <Image w={{ base: "100%", md: "650px"}} position={{ base: "static", md: "absolute"}} bottom={{ base: "auto", md: 10}} left={{ base: "auto", md: 10}} src={`${process.env.PUBLIC_URL}/img/kobe2_mono.jpg`} alt="神戸の夜景"/>
+                {/* </Box> */}
             </Box>
         </Box>
     )
