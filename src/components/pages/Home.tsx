53ca1d619fef4a4e17b7fd49814c65a1bd1ceb32
yuomatsu <07yuya08@gmail.com>
2022-02-26 14:31:24 +0900

    HOMEを修正、PLOFILE作成開始

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,11 +7,13 @@ import { Image, Text } from '@chakra-ui/react';
 export const Home: VFC = memo(() => {
     return (
         <Box mt={{ base: 5, md: 120}} mb={{ base: 5, md: 20}} mr={{ base: 5, md: "auto"}} ml={{ base: 5, md: "auto"}} textAlign="center" backgroundColor="gray.300" maxW="1200px">
-            <Text fontSize='5xl'>Welcome to my portfolio!</Text>
-            <Text fontSize='5xl'>Thank you!</Text>
-            <Box mt={{ base:5, md: 120}} w="100%" h="900px" position={{ base: "static", md: "relative"}}>
+            <Box mt={{ base: 20, md: 120}} fontSize={{ base: '1.5rem', md: "3rem"}}>
+                <Text>Welcome to my portfolio!</Text>
+                <Text>Thank you!</Text>
+            </Box>
+            <Box mt={{ base: 20, md: 120}} mb={{ base: 20, md: 0 }} w="100%" h={{ base: "auto", md: "900px"}} position={{ base: "static", md: "relative"}}>
                 {/* <Box position={{ base: "static", md: "relative"}} w="100%" h="700px"> */}
-                <Image w={{ base: "100%", md: "650px"}} position={{ base: "static", md: "absolute"}} top={{ base: "auto", md: 0}} right={{ base: "auto", md: 10}} src={`${process.env.PUBLIC_URL}/img/kobe1_mono.jpg`} alt="神戸のスタバ"/>
+                <Image mb={{ base: 20, md: 0 }} w={{ base: "100%", md: "650px"}} position={{ base: "static", md: "absolute"}} top={{ base: "auto", md: 0}} right={{ base: "auto", md: 10}} src={`${process.env.PUBLIC_URL}/img/kobe1_mono.jpg`} alt="神戸のスタバ"/>
                 <Image w={{ base: "100%", md: "650px"}} position={{ base: "static", md: "absolute"}} bottom={{ base: "auto", md: 10}} left={{ base: "auto", md: 10}} src={`${process.env.PUBLIC_URL}/img/kobe2_mono.jpg`} alt="神戸の夜景"/>
                 {/* </Box> */}
             </Box>
--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -1,5 +1,33 @@
 import { memo, VFC } from "react";
+import { Box } from "@chakra-ui/layout";
+import { Image, Text } from '@chakra-ui/react';
 
 export const Profile: VFC = memo(() => {
-    return <p>プロフィールページです</p>
+    return (
+        <Box mt={{ base: 5, md: 120}} mb={{ base: 5, md: 20}} mr={{ base: 5, md: "auto"}} ml={{ base: 5, md: "auto"}} textAlign="center" backgroundColor="gray.300" maxW="1200px">
+            <Box mt={{ base: 20, md: 120}} fontSize={{ base: '1.5rem', md: "3rem"}}>
+                <Text>PLOFILE</Text>
+            </Box>
+            <Box mt={{ base: 5, md: 120}} mb={{ base: 5, md: 20}} mr={{ base: 0, md: "auto"}} ml={{ base: 0, md: "auto"}} w="100%">
+                <Image mb={{ base: 20, md: 0 }} mr={{ base: 0, md: "auto"}} ml={{ base: 0, md: "auto"}} w={{ base: "50vw", md: "300px"}} src={`${process.env.PUBLIC_URL}/img/me_mono.jpg`} alt="yuomatsuの後ろ姿"/>
+            </Box>
+            <Text>Web Engineer</Text>
+            <Box>
+                <Text textAlign="left">
+                    はじめましてyuomatsuと申します。<br />
+                    都内のITコンサルでWebエンジニアとして働いています。<br />
+                    フロントエンドとバックエンドどちらの開発も担当しています。<br />
+                    より良いサービスを開発・運用できるように日々奮闘しています。<br />
+                </Text>
+            </Box>
+            <Box>
+                <Text as="h2">Age</Text>
+                <Text>28歳</Text>
+                <Text as="h2">Hobby</Text>
+                <Text>28歳</Text>
+                <Text as="h2">Favorite Food</Text>
+                <Text>28歳</Text>
+            </Box>
+        </Box>
+    )
 }) 
